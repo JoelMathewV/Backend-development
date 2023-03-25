3a0c574abc3b2af95cb758f225eb60d2206303f6
Joel Mathew V <joelmathewv0819@gmail.com>
2023-03-25 18:04:25 +0530

    multiple lists

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 var items = [];
+var works = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -23,9 +24,25 @@ app.get("/", function(req, res){
 
 app.post("/",function(req, res){
     var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if (req.body.button === "Work"){
+        works.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 } );
+
+app.get("/work", function(req, res){
+    res.render("list", {kindofday: "Work Day", itemName: works});
+});
+
+app.post("/work", function(req,res){
+    let item = req.body.newItem;
+    works.push(item);
+    res.redirect("/work");
+});
 
 app.listen(3000, function(){
     console.log("server started on port 3000");
